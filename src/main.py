c6a9d1661ce943cbebd4b9bf772c76bb77836091
Towster15 <105541074+Towster15@users.noreply.github.com>
2024-04-25 16:08:23 +0100

    Move GUI to separate thread

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import queue
 import pyvirtualcam
 import numpy as np
+from threading import Thread
 
 from gui import GUI
 from kinect import Kinect
@@ -10,13 +11,15 @@ if __name__ == "__main__":
     video_frame_queue = queue.Queue()
     tilt_queue = queue.Queue()
 
-    gui = GUI(tilt_queue)
     kinect = Kinect(video_frame_queue)
+
+    gui_thread = Thread(target=GUI, args=[tilt_queue])
+    gui_thread.start()
 
     with pyvirtualcam.Camera(width=640, height=480, fps=30) as vcam:
         print(f"Using virtual camera: {vcam.device}")
         default_frame = np.zeros((vcam.height, vcam.width, 3), np.uint8)
-        while True:
+        while gui_thread.is_alive():
             try:
                 kinect.adjust_angle(tilt_queue.get_nowait())
             except queue.Empty:
@@ -29,3 +32,5 @@ if __name__ == "__main__":
                 default_frame[:] = (c * 100, c * 100, c * 100)
                 vcam.send(default_frame)
             vcam.sleep_until_next_frame()
+
+    kinect.close()
